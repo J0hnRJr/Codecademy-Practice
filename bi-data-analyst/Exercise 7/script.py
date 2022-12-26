f4b7c9a7f0c3b18114c272141e1852df93508f68
J0hnRjr <jlrenodin@liberty.edu>
2022-12-26 15:51:55 -0500

    modify code as instructed

--- bi-data-analyst/Exercise 7/script.py
+++ bi-data-analyst/Exercise 7/script.py
@@ -1,8 +1,9 @@
 # All lines provided by Codecademy
 # Change these numbers and run to predict species!
 
-mystery_penguin_flipper = 180
-mystery_penguin_bill = 35
+# These two values were modified by John Renodin
+mystery_penguin_flipper = 260
+mystery_penguin_bill = 90
 
 # The model will use those values and the dataset to predict a species
 from code import predict
