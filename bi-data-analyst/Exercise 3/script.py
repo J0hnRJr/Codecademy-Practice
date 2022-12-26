bc834d48601a3c48c6fb6cdbb9bff6f5afd47909
J0hnRjr <jlrenodin@liberty.edu>
2022-12-26 15:25:39 -0500

    add provided code for exercise 3

---
+++ bi-data-analyst/Exercise 3/script.py
@@ -0,0 +1,17 @@
+# All lines provided by Codecademy
+libraries = ["NumPy", "SciPy", "Pandas", "Matplotlib", "Seaborn"]
+completion = [100, 100, 96, 0, 0]
+
+libraries.append("scikit-learn")
+completion.append(0)
+
+gradebook = list(zip(libraries, completion))
+
+print("Lesson Completion Rates:")
+print(gradebook)
+print("\n")
+
+# What's next?
+
+# gradebook.append(("BeautifulSoup", 0))
+# gradebook.append(("Tensorflow", 0))
