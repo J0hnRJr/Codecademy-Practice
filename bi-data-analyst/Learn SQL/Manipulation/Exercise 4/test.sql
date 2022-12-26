48b379999821965fd70aac9e4562697bc1d11053
J0hnRjr <jlrenodin@liberty.edu>
2022-12-26 16:26:54 -0500

    add Manipulation/Exercise 4 step 2

--- bi-data-analyst/Learn SQL/Manipulation/Exercise 4/test.sql
+++ bi-data-analyst/Learn SQL/Manipulation/Exercise 4/test.sql
@@ -3,3 +3,18 @@ INSERT INTO
     celebs (id, name, age)
 VALUES
     (1, 'Justin Bieber', 22);
+
+INSERT INTO
+    celebs (id, name, age)
+VALUES
+    (2, 'Beyonce Knowles', 33);
+
+INSERT INTO
+    celebs (id, name, age)
+VALUES
+    (3, 'Jeremy Lin', 26);
+
+INSERT INTO
+    celebs (id, name, age)
+VALUES
+    (4, 'Taylor Swift', 26);
