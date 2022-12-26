9e5505d193465a38862e410f8c4e7c05dc8ce314
J0hnRjr <jlrenodin@liberty.edu>
2022-12-26 16:44:13 -0500

    add Project_1.sql step 2

--- bi-data-analyst/Learn SQL/Manipulation/Project/Project_1.sql
+++ bi-data-analyst/Learn SQL/Manipulation/Project/Project_1.sql
@@ -1,3 +1,11 @@
 -- All lines written by John Renodin
 CREATE TABLE
     friends (INTEGER id, TEXT name, DATE birthday);
+
+GO
+INSERT INTO
+    friends (id, name, birthday)
+VALUES
+    (1, 'Ororo Munroe', '05/30/1940');
+
+GO
