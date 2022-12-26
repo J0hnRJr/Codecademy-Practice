8dfb73134529c31e792b25ab8e1aaa6f0d6179c6
J0hnRjr <jlrenodin@liberty.edu>
2022-12-26 15:39:51 -0500

    modify number

--- bi-data-analyst/Exercise 6/script.py
+++ bi-data-analyst/Exercise 6/script.py
@@ -4,6 +4,6 @@
 
 from simulate import simulate
 
-num_people_in_room = 2 			#Change This Number (keep it smaller than 100 to save processing power)
+num_people_in_room = 99 # Changed by John Renodin #Change This Number (keep it smaller than 100 to save processing power)
 
 simulate(num_people_in_room) 
