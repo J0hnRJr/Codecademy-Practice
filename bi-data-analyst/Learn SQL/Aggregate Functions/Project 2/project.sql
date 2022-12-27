20bf87fc1bbba258b7eae20133179b378bf1bb46
J0hnRjr <jlrenodin@liberty.edu>
2022-12-27 16:22:20 -0500

    Add Aggregate functions/Project 2/step 10

--- bi-data-analyst/Learn SQL/Aggregate Functions/Project 2/project.sql
+++ bi-data-analyst/Learn SQL/Aggregate Functions/Project 2/project.sql
@@ -1,11 +1,7 @@
 --All lines written by John Renodin
 SELECT
-    CASE
-        WHEN url LIKE '%github.com%' THEN 'GitHub'
-        WHEN url LIKE '%medium.com%' THEN 'Medium'
-        WHEN url LIKE '%nytimes.com%' THEN 'New York Times'
-        ELSE 'Other'
-    END AS 'Source',
+    strftime ('%H', TIMESTAMP) AS 'Hour',
+    AVG(score),
     COUNT(*)
 FROM
     hacker_news
