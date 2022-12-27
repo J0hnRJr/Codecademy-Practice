32044cca69ad020e65aad85824b9b2af94268145
J0hnRjr <jlrenodin@liberty.edu>
2022-12-27 10:49:41 -0500

    Add queries/exercise 10/step 2

--- bi-data-analyst/Learn SQL/Queries/Exercise 10/and.sql
+++ bi-data-analyst/Learn SQL/Queries/Exercise 10/and.sql
@@ -1,6 +1,8 @@
---All lines provided by Codecademy
-SELECT *
-FROM movies
-WHERE 
-    year BETWEEN 1970 AND 1979
-  AND imdb_rating > 8
+--All written by John Renodin
+SELECT
+    *
+FROM
+    movies
+WHERE
+    YEAR < 1985
+    AND genre = 'horror';
