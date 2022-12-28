ed83cce74cced687c6af2a3e737fa35868bd578e
J0hnRjr <jlrenodin@liberty.edu>
2022-12-28 16:56:28 -0500

    Python/Introduction/project 1 step 9

--- bi-data-analyst/Python/Part I/Hello World/Project 1/script.py
+++ bi-data-analyst/Python/Part I/Hello World/Project 1/script.py
@@ -21,7 +21,11 @@ print("The change in cost of insurance after increasing the age by 4 years is " 
 # BMI Factor
 age = 28
 bmi += 3.1
-
+new_insurance_cost = 50 * age - 128 * sex \
+  + 370 * bmi + 425 * num_of_children \
+  + 24000 * smoker - 12500
+change_in_insurance_cost = new_insurance_cost - insurance_cost  
+print("The change in estimated insurance cost after increasing BMI by 3.1 is " + str(change_in_insurance_cost) + " dollars.")
 # Male vs. Female Factor
 
 
