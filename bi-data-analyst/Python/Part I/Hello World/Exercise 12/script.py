42ba916c5338c70197740179c8d8e4a78ad2458c
J0hnRjr <jlrenodin@liberty.edu>
2022-12-28 16:23:02 -0500

    Python/Introduction/exercise 12 provided code

---
+++ bi-data-analyst/Python/Part I/Hello World/Exercise 12/script.py
@@ -0,0 +1,12 @@
+# All lines provided by Codecademy
+string1 = "The wind, "
+string2 = "which had hitherto carried us along with amazing rapidity, "
+string3 = "sank at sunset to a light breeze; "
+string4 = "the soft air just ruffled the water and "
+string5 = "caused a pleasant motion among the trees as we approached the shore, "
+string6 = "from which it wafted the most delightful scent of flowers and hay."
+
+# Define message below:
+
+
+#print(message)
