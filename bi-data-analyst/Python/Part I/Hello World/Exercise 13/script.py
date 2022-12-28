33102ceb40bb8158d7b71fe6c492c74d535887a3
J0hnRjr <jlrenodin@liberty.edu>
2022-12-28 16:26:13 -0500

    Python/Introduction/exercise 13 provided code

---
+++ bi-data-analyst/Python/Part I/Hello World/Exercise 13/script.py
@@ -0,0 +1,12 @@
+# All lines provided by Codecademy
+total_price = 0
+
+new_sneakers = 50.00
+
+total_price += new_sneakers
+
+nice_sweater = 39.00
+fun_books = 20.00
+# Update total_price here:
+
+print("The total price is", total_price)
