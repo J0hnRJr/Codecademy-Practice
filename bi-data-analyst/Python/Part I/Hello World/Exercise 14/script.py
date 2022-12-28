48aec3efd3b0c7f42e0847b03e4a0d350e8f56de
J0hnRjr <jlrenodin@liberty.edu>
2022-12-28 16:30:10 -0500

    Python/Introduction/exercise 14 modified code

--- bi-data-analyst/Python/Part I/Hello World/Exercise 14/script.py
+++ bi-data-analyst/Python/Part I/Hello World/Exercise 14/script.py
@@ -1,6 +1,6 @@
-# All lines provided by Codecademy
+# All lines provided by Codecademy. Lines 3-5 modified by John Renodin.
 # Assign the string here
-
-
-
+to_you = '''Stranger, if you passing meet me and desire to speak to me, why
+  should you not speak to me?
+And why should I not speak to you?'''
 print(to_you)
