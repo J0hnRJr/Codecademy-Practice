ec4cc46e1889d8c9e3253d3519a9accc796a65df
J0hnRjr <jlrenodin@liberty.edu>
2022-12-28 16:02:00 -0500

    Python/Introduction/exercise 5

---
+++ bi-data-analyst/Python/Part I/Introduction/Exercise 5/script.py
@@ -0,0 +1,20 @@
+# All lines provdied by Codecademy. Line 10 and 17 modified by John Renodin
+# We've defined the variable "meal" here to the name of the food we ate for breakfast!
+meal = "An english muffin"
+
+# Printing out breakfast
+print("Breakfast:")
+print(meal)
+
+# Now update meal to be lunch!
+meal = 'Caeser Salad' # modified by John Renodin
+
+# Printing out lunch
+print("Lunch:")
+print(meal)
+
+# Now update "meal" to be dinner
+meal = 'Steak' # modified by John Renodin
+# Printing out dinner
+print("Dinner:")
+print(meal)
