3ab321bfbfecfed463f3afbd9d7c78f74434754c
J0hnRjr <jlrenodin@liberty.edu>
2022-12-28 16:49:31 -0500

    Python/Introduction/project 1 step 5

--- bi-data-analyst/Python/Part I/Hello World/Project 1/script.py
+++ bi-data-analyst/Python/Part I/Hello World/Project 1/script.py
@@ -12,8 +12,10 @@ insurance_cost = 250 * age - 128 * sex \
   + 24000 * smoker - 12500
 print("This person's insurance cost is " + str(insurance_cost) + " dollars.")
 # Age Factor
-
-
+age += 4
+new_insurance_cost = 50 * age - 128 * sex \
+  + 370 * bmi + 425 * num_of_children \
+  + 24000 * smoker - 12500
 # BMI Factor
 
 
