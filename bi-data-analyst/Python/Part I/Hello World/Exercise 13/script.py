efb30065961a2cf7eb1a8675a60d97d0d85b67ae
J0hnRjr <jlrenodin@liberty.edu>
2022-12-28 16:27:26 -0500

    Python/Introduction/exercise 13 modified code

--- bi-data-analyst/Python/Part I/Hello World/Exercise 13/script.py
+++ bi-data-analyst/Python/Part I/Hello World/Exercise 13/script.py
@@ -1,4 +1,4 @@
-# All lines provided by Codecademy
+# All lines provided by Codecademy. Line 11 modified by John Renodin
 total_price = 0
 
 new_sneakers = 50.00
@@ -8,5 +8,5 @@ total_price += new_sneakers
 nice_sweater = 39.00
 fun_books = 20.00
 # Update total_price here:
-
+total_price += nice_sweater + fun_books
 print("The total price is", total_price)
