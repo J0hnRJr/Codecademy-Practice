2928ace9ac9bb3b40e66ff6b002de0de390467b6
J0hnRjr <jlrenodin@liberty.edu>
2022-12-28 16:38:40 -0500

    Python/Introduction/project 1 step 0

---
+++ bi-data-analyst/Python/Part I/Hello World/Project 1/script.py
@@ -0,0 +1,17 @@
+# All lines provided by Codecademy
+# create the initial variables below
+
+
+# Add insurance estimate formula below
+
+
+# Age Factor
+
+
+# BMI Factor
+
+
+# Male vs. Female Factor
+
+
+# Extra Practice
