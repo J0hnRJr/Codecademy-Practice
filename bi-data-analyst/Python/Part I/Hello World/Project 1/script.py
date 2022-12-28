ec241cd3ed4eec13ece5200fc0dc36e2457c8a0e
J0hnRjr <jlrenodin@liberty.edu>
2022-12-28 16:42:50 -0500

    Python/Introduction/project 1 step 1

--- bi-data-analyst/Python/Part I/Hello World/Project 1/script.py
+++ bi-data-analyst/Python/Part I/Hello World/Project 1/script.py
@@ -1,6 +1,10 @@
 # All lines provided by Codecademy
 # create the initial variables below
-
+age = 28
+sex = 0
+bmi = 26.2
+num_of_children = 3
+smoker = 0
 
 # Add insurance estimate formula below
 
