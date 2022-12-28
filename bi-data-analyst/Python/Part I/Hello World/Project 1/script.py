3dca050fc00026aaab1ab02b6bb3e748cfdefaaa
J0hnRjr <jlrenodin@liberty.edu>
2022-12-28 16:47:07 -0500

    Python/Introduction/project 1 step 2

--- bi-data-analyst/Python/Part I/Hello World/Project 1/script.py
+++ bi-data-analyst/Python/Part I/Hello World/Project 1/script.py
@@ -7,7 +7,9 @@ num_of_children = 3
 smoker = 0
 
 # Add insurance estimate formula below
-
+insurance_cost = 250 * age - 128 * sex \
+  + 370 * bmi + 425 * num_of_children \
+  + 24000 * smoker - 12500
 
 # Age Factor
 
