49c98993c331d56a44000c37ae0d8c6238463955
J0hnRjr <jlrenodin@liberty.edu>
2022-12-28 13:23:34 -0500

    Add Multiple Tables/Project 1 step 4

--- bi-data-analyst/Learn SQL/Multiple Tables/Project 1/test.sql
+++ bi-data-analyst/Learn SQL/Multiple Tables/Project 1/test.sql
@@ -1,6 +1,13 @@
 -- All lines written by John Renodin
 SELECT
-    *
+    trips.date,
+    trips.pickup,
+    trips.dropoff,
+    trips.type,
+    trips.cost,
+    riders.first,
+    riders.last,
+    riders.username
 FROM
-    riders
-    CROSS JOIN cars;
+    trips
+    LEFT JOIN riders ON trips.rider_id = riders.id;
