21d12eacbf6cbdd09ea1b28b5ddd230b6b435073
J0hnRjr <jlrenodin@liberty.edu>
2022-12-28 13:25:51 -0500

    Add Multiple Tables/Project 1 step 5

--- bi-data-analyst/Learn SQL/Multiple Tables/Project 1/test.sql
+++ bi-data-analyst/Learn SQL/Multiple Tables/Project 1/test.sql
@@ -1,13 +1,6 @@
 -- All lines written by John Renodin
 SELECT
-    trips.date,
-    trips.pickup,
-    trips.dropoff,
-    trips.type,
-    trips.cost,
-    riders.first,
-    riders.last,
-    riders.username
+    *
 FROM
     trips
-    LEFT JOIN riders ON trips.rider_id = riders.id;
+    INNER JOIN cars ON trips.car_id = cars.id;
