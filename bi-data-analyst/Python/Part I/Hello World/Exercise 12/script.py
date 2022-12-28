b66348ff235e1bdf19bc121f79a76d20bb4471da
J0hnRjr <jlrenodin@liberty.edu>
2022-12-28 16:24:46 -0500

    Python/Introduction/exercise 12 modified code

--- bi-data-analyst/Python/Part I/Hello World/Exercise 12/script.py
+++ bi-data-analyst/Python/Part I/Hello World/Exercise 12/script.py
@@ -1,4 +1,4 @@
-# All lines provided by Codecademy
+# All lines provided by Codecademy. Line 10 modified by John Renodin
 string1 = "The wind, "
 string2 = "which had hitherto carried us along with amazing rapidity, "
 string3 = "sank at sunset to a light breeze; "
@@ -7,6 +7,6 @@ string5 = "caused a pleasant motion among the trees as we approached the shore, 
 string6 = "from which it wafted the most delightful scent of flowers and hay."
 
 # Define message below:
-
+message = string1 + string2 + string3 + string4 + string5 + string6
 
 #print(message)
