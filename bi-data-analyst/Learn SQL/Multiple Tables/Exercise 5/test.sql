2070c86fc982c9db9364e90fa1082e3de40e4189
J0hnRjr <jlrenodin@liberty.edu>
2022-12-28 11:29:40 -0500

    Add Multiple Tables/Exercise 5 step 2

--- bi-data-analyst/Learn SQL/Multiple Tables/Exercise 5/test.sql
+++ bi-data-analyst/Learn SQL/Multiple Tables/Exercise 5/test.sql
@@ -4,3 +4,11 @@ SELECT
 FROM
     newspaper
     LEFT JOIN online ON newspaper.id = online.id;
+
+SELECT
+    *
+FROM
+    newspaper
+    LEFT JOIN online ON newspaper.id = online.id
+WHERE
+    online.id IS NULL;
