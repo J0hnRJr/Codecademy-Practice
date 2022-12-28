355a52d85fb0afca1865e0f21d89d583443cee30
J0hnRjr <jlrenodin@liberty.edu>
2022-12-28 16:08:58 -0500

    Python/Introduction/exercise 7 step 1

---
+++ bi-data-analyst/Python/Part I/Introduction/Exercise 7/script.py
@@ -0,0 +1,6 @@
+# All SLOC written by John Renodin Comments provided by Codecademy
+# Define the release and runtime integer variables below:
+release_year = 1990
+runtime = 2
+
+# Define the rating_out_of_10 float variable below: 
