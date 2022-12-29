4be5b111679c2cf3b73f6791be7e9f615331b010
J0hnRjr <jlrenodin@liberty.edu>
2022-12-29 15:05:16 -0500

    Python/Functions/Exercise 12 step 4

--- bi-data-analyst/Python/Part I/Functions/Exercise 12/travel.py
+++ bi-data-analyst/Python/Part I/Functions/Exercise 12/travel.py
@@ -6,4 +6,8 @@ def estimated_time_rounded(time):
   return round(time)
 estimate = estimated_time_rounded(5.6)
 def destination_setup(origin, destination, estimated_time, mode_of_transport='Car'):
-  
+  print('Your trip starts off in ' + str(origin))
+  print('And you are traveling to' + str(destination))
+  print('You will be traveling by ' + str(mode_of_transport))
+  print('It will take approximately ' + str(estimated_time) + ' hours')
+destination_setup('home', 'Codecademy HQ', estimate, 'Jetblue')
