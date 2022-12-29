d6cbd233bc299dbe0858d678af357fc0f73f1380
J0hnRjr <jlrenodin@liberty.edu>
2022-12-29 14:27:30 -0500

    add newline to comment

--- bi-data-analyst/Python/Part I/Functions/writeFiles.py
+++ bi-data-analyst/Python/Part I/Functions/writeFiles.py
@@ -4,5 +4,5 @@ parentDir = 'c:/dev/Codecademy-Practice/bi-data-analyst/Python/Part I/Functions'
 exerciseNum = 6
 for num in range(exerciseNum, 13):
     f = open(parentDir + '/Exercise ' + str(num) + '/' + "travel.py", "w")
-    f.write('# All lines written by John Renodin')
+    f.write('# All lines written by John Renodin\n')
     f.close()
