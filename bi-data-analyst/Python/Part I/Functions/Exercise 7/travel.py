d0dcbb47ccd50d4cf519a7a2a10fae9f1ceffe72
J0hnRjr <jlrenodin@liberty.edu>
2022-12-29 13:31:53 -0500

    Python/Functions/Exercise 7 step 4

--- bi-data-analyst/Python/Part I/Functions/Exercise 7/travel.py
+++ bi-data-analyst/Python/Part I/Functions/Exercise 7/travel.py
@@ -9,3 +9,4 @@ def trip_planner(
     ', then ' + str(second_destination) + \
     ', and lastly ' + str(final_destination))
 trip_planner("France", "Germany", "Denmark")
+trip_planner("Denmark", "France", "Germany")
