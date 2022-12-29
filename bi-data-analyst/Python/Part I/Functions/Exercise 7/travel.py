2df7ff44c149aa28fa13da785870f97f06b14dcd
J0hnRjr <jlrenodin@liberty.edu>
2022-12-29 13:29:49 -0500

    Python/Functions/Exercise 7 step 3

--- bi-data-analyst/Python/Part I/Functions/Exercise 7/travel.py
+++ bi-data-analyst/Python/Part I/Functions/Exercise 7/travel.py
@@ -5,3 +5,6 @@ def trip_planner(
   final_destination = "Codecademy HQ"
 ):
     print('Here is what your trip will look like!')
+    print('First, we will stop in ' + str(first_destination) + \
+    ', then ' + str(second_destination) + \
+    ', and lastly ' + str(final_destination))
