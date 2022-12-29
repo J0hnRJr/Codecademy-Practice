5c27540eb91fa40cc9927216071a6c15cdfafa16
J0hnRjr <jlrenodin@liberty.edu>
2022-12-29 12:32:35 -0500

    Python/Functions/Exercise 2 step 2

--- bi-data-analyst/Python/Part I/Functions/Exercise 2/travel.py
+++ bi-data-analyst/Python/Part I/Functions/Exercise 2/travel.py
@@ -1,3 +1,5 @@
 # All lines written by John Renodin
 def directions_to_timesSq():
-  
+  print('Walk 4 mins to 34th St Herald Square train station')
+  print('Take the Northbound N, Q, R, or W train 1 stop')
+  print('Get off the Times Square 42nd Street stop')
