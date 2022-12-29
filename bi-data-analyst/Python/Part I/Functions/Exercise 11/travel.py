6bef9640519b949f438f444f244258f74257f04e
J0hnRjr <jlrenodin@liberty.edu>
2022-12-29 14:54:53 -0500

    Python/Functions/Exercise 11 step 3

--- bi-data-analyst/Python/Part I/Functions/Exercise 11/travel.py
+++ bi-data-analyst/Python/Part I/Functions/Exercise 11/travel.py
@@ -4,3 +4,4 @@ def top_tourist_locations_italy():
   second = "Venice"
   third = "Florence"
   return first, second, third
+most_popular1, most_popular2, most_popular3 = top_tourist_locations_italy()
