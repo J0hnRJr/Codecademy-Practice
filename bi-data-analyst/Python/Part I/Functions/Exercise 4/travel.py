b13200ddabdd1e9fe62b10c6e31a985f6fbdecd7
J0hnRjr <jlrenodin@liberty.edu>
2022-12-29 12:40:20 -0500

    Python/Functions/Exercise 4 step 4

--- bi-data-analyst/Python/Part I/Functions/Exercise 4/travel.py
+++ bi-data-analyst/Python/Part I/Functions/Exercise 4/travel.py
@@ -3,3 +3,4 @@ print('Checking the weather for you!')
 def weather_check():
   print('Looks great outside! Enjoy your trip.')
   print('False Alarm, the weather changed! There is a thunderstorm approaching. Cancel your plans and stay inside.')
+weather_check()
