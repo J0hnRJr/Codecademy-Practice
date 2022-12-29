337abc2aee4e8e8571918f3bb0989f4e620086a5
J0hnRjr <jlrenodin@liberty.edu>
2022-12-29 12:35:35 -0500

    Python/Functions/Exercise 3 step 2

--- bi-data-analyst/Python/Part I/Functions/Exercise 3/travel.py
+++ bi-data-analyst/Python/Part I/Functions/Exercise 3/travel.py
@@ -1,8 +1,9 @@
-# All lines provided by Codecademy
+# All lines provided by Codecademy. Modified by John Renodin.
 def directions_to_timesSq():
   print("Walk 4 mins to 34th St Herald Square train station.")
   print("Take the Northbound N, Q, R, or W train 1 stop.")
   print("Get off the Times Square 42nd Street stop.")
+  print('Take lots of pictures!')
 
 # Call your function here:
 directions_to_timesSq()
