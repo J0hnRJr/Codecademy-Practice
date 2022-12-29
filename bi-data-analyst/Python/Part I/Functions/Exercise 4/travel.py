717a2598a0dc3b6154293728e0841633682c21bf
J0hnRjr <jlrenodin@liberty.edu>
2022-12-29 12:39:45 -0500

    Python/Functions/Exercise 4 step 3

--- bi-data-analyst/Python/Part I/Functions/Exercise 4/travel.py
+++ bi-data-analyst/Python/Part I/Functions/Exercise 4/travel.py
@@ -1,2 +1,5 @@
 # All lines written by John Renodin.
 print('Checking the weather for you!')
+def weather_check():
+  print('Looks great outside! Enjoy your trip.')
+  print('False Alarm, the weather changed! There is a thunderstorm approaching. Cancel your plans and stay inside.')
