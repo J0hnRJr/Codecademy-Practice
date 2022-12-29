4f26a4eac50e90fde004bbbcfd031a570b70034a
J0hnRjr <jlrenodin@liberty.edu>
2022-12-29 13:13:25 -0500

    Python/Functions/Exercise 5 step 3

--- bi-data-analyst/Python/Part I/Functions/Exercise 5/travel.py
+++ bi-data-analyst/Python/Part I/Functions/Exercise 5/travel.py
@@ -1,3 +1,4 @@
 # All lines written by John Renodin
 def generate_trip_instructions(location):
-    print("Looks like you are planning a trip to visit " +str(location))
+    print("Looks like you are planning a trip to visit " + str(location))
+    print("You can use the public subway system to get to " + str(location))
