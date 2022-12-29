027b70106884824be69762a8e3914928046f2e79
J0hnRjr <jlrenodin@liberty.edu>
2022-12-29 12:29:25 -0500

    Python/Functions/Exercise 1 step 2

--- bi-data-analyst/Python/Part I/Functions/Exercise 1/travel.py
+++ bi-data-analyst/Python/Part I/Functions/Exercise 1/travel.py
@@ -1,15 +1,17 @@
-# All lines provided by Codecademy
+# All lines provided by Codecademy. Modified by John Renodin
 # First user wants to travel between these two points!
 print("Setting the Empire State Building as the starting point and Times Square as our destination.")
 print("Calculating the total distance between our points.") 
 print("The best route is by train and will take approximately 10 minutes.") 
-
 # Second user wants to travel between these two points!
-
-
-
+print("Setting the Empire State Building as the starting point and Times Square as our destination.")
+print("Calculating the total distance between our points.") 
+print("The best route is by train and will take approximately 10 minutes.") 
 # Third user wants to travel between these two points!
-
-
-
+print("Setting the Empire State Building as the starting point and Times Square as our destination.")
+print("Calculating the total distance between our points.") 
+print("The best route is by train and will take approximately 10 minutes.") 
 # Fourth user wants to travel between these two points!
+print("Setting the Empire State Building as the starting point and Times Square as our destination.")
+print("Calculating the total distance between our points.") 
+print("The best route is by train and will take approximately 10 minutes.") 
