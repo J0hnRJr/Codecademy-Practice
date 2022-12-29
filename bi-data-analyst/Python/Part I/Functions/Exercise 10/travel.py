e675679e7082f37105a90bb2b1cb386d2ea4ece2
J0hnRjr <jlrenodin@liberty.edu>
2022-12-29 14:28:20 -0500

    Python/Functions/Exercise 10 step 1

---
+++ bi-data-analyst/Python/Part I/Functions/Exercise 10/travel.py
@@ -0,0 +1,10 @@
+# All lines provided by Codecademy
+current_budget = 3500.75
+
+def print_remaining_budget(budget):
+  print("Your remaining budget is: $" + str(budget))
+
+print_remaining_budget(current_budget)
+
+# Write your code below: 
+
