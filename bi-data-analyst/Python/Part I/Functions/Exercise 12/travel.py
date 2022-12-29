3f7a378a2c6bf6c26e9f1c630b5bf96c19a34ce0
J0hnRjr <jlrenodin@liberty.edu>
2022-12-29 15:00:36 -0500

    Python/Functions/Exercise 12 step 2

--- bi-data-analyst/Python/Part I/Functions/Exercise 12/travel.py
+++ bi-data-analyst/Python/Part I/Functions/Exercise 12/travel.py
@@ -2,3 +2,6 @@
 def trip_planner_welcome(name):
   print('Welcome to tripplanner v1.0 ' + str(name))
 trip_planner_welcome("J0hnRJr")
+def estimated_time_rounded(time):
+  return round(time)
+estimate = estimated_time_rounded(5.6)
