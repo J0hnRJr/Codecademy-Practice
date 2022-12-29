bfbce03c70264586e0c67ff0ccf1fee65fbb2015
J0hnRjr <jlrenodin@liberty.edu>
2022-12-29 13:15:05 -0500

    Python/Functions/Exercise 5 step 5

--- bi-data-analyst/Python/Part I/Functions/Exercise 5/travel.py
+++ bi-data-analyst/Python/Part I/Functions/Exercise 5/travel.py
@@ -2,4 +2,4 @@
 def generate_trip_instructions(location):
     print("Looks like you are planning a trip to visit " + str(location))
     print("You can use the public subway system to get to " + str(location))
-generate_trip_instructions("Central Park")
+generate_trip_instructions("Grand Central Station")
