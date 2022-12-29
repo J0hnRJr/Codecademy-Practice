0c63693e6887eb6e7f5f17b9d73ba41892303a9f
J0hnRjr <jlrenodin@liberty.edu>
2022-12-29 14:53:47 -0500

    Python/Functions/Exercise 11 step 2

--- bi-data-analyst/Python/Part I/Functions/Exercise 11/travel.py
+++ bi-data-analyst/Python/Part I/Functions/Exercise 11/travel.py
@@ -1,5 +1,6 @@
-# All lines provided by Codecademy
+# Lines 2-5 provided by Codecademy. All other lines written by John Renodin
 def top_tourist_locations_italy():
   first = "Rome"
   second = "Venice"
   third = "Florence"
+  return first, second, third
