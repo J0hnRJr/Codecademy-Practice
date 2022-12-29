69ef004abce2d4003e09ec119cf35b1ca9575bd4
J0hnRjr <jlrenodin@liberty.edu>
2022-12-29 14:18:20 -0500

    Python/Functions/Exercise 8 step 1

--- bi-data-analyst/Python/Part I/Functions/Exercise 8/travel.py
+++ bi-data-analyst/Python/Part I/Functions/Exercise 8/travel.py
@@ -5,3 +5,6 @@ mug_price = 5.99
 poster_price = 2.00
 
 # Write your code below:
+# Lines below written by John Renodin
+max_price = max(tshirt_price, shorts_price, mug_price, poster_price)
+print(max_price)
