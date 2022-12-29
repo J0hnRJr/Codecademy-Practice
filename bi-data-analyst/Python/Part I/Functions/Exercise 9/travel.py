ff52eb069e038bea3ceb685590d8954b95dd09f5
J0hnRjr <jlrenodin@liberty.edu>
2022-12-29 14:24:19 -0500

    Python/Functions/Exercise 9 step 0

---
+++ bi-data-analyst/Python/Part I/Functions/Exercise 9/travel.py
@@ -0,0 +1,12 @@
+# All lines provided by Codecademy
+# This function will print a hardcoded count of how many locations we have.
+def print_count_locations():
+  favorite_locations = "Paris, Norway, Iceland"
+  print("There are 3 locations")
+    
+# This function will print the favorite locations
+def show_favorite_locations():
+  print("Your favorite locations are: " + favorite_locations)
+
+print_count_locations()
+show_favorite_locations()
