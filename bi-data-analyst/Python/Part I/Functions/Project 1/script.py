25ca2d9444acb258c70c009b886baf111d509be3
J0hnRjr <jlrenodin@liberty.edu>
2022-12-29 15:28:20 -0500

    Python/Functions/Project1 step 9

--- bi-data-analyst/Python/Part I/Functions/Project 1/script.py
+++ bi-data-analyst/Python/Part I/Functions/Project 1/script.py
@@ -1,12 +1,12 @@
 # All lines provided by Codecademy execept lines 3-4 and other modifications by John Renodin
 # Create calculate_insurance_cost() function below: 
-def insurance_cost(age, sex, bmi, num_of_children, smoker):
+def insurance_cost(age, sex, bmi, num_of_children, smoker, name):
   estimated_cost = 250*age - 128*sex + 370*bmi + 425*num_of_children + 24000*smoker - 12500
-  print('The estimated insurance cost for this person is ' + str(estimated_cost) + ' dollars.')
+  print('The estimated insurance cost for ' + str(name) + ' is ' + str(estimated_cost) + ' dollars.')
   return estimated_cost
 
 # Estimate Maria's insurance cost
-maria_insurance_cost = insurance_cost(28, 0, 26.2, 3, 0)
+maria_insurance_cost = insurance_cost(28, 0, 26.2, 3, 0, "Maria")
 
 # Estimate Omar's insurance cost 
-omar_insurance_cost = insurance_cost(35,1,22.2,0,1)
+omar_insurance_cost = insurance_cost(35,1,22.2,0,1, "Omar")
