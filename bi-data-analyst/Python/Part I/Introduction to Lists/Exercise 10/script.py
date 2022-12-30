ea5b4b8776649b4aa28eb4bdedf11cb274e18ad1
J0hnRjr <jlrenodin@liberty.edu>
2022-12-30 13:48:40 -0500

    Python/Lists/Exercise 10 step 5

--- bi-data-analyst/Python/Part I/Introduction to Lists/Exercise 10/script.py
+++ bi-data-analyst/Python/Part I/Introduction to Lists/Exercise 10/script.py
@@ -6,4 +6,5 @@ print(order_list)
 
 new_store_order_list = ["Orange", "Apple", "Mango", "Broccoli", "Mango"]
 new_store_order_list.remove("Mango")
+new_store_order_list.remove("Onions") # causes an error
 print(new_store_order_list)
