27a7108f209f64b4c17d181726ba4b75c8edb1e6
J0hnRjr <jlrenodin@liberty.edu>
2022-12-30 14:02:05 -0500

    Python/Lists/Exercise 13 step 1

--- bi-data-analyst/Python/Part I/Introduction to Lists/Exercise 13/script.py
+++ bi-data-analyst/Python/Part I/Introduction to Lists/Exercise 13/script.py
@@ -1,2 +1,8 @@
-# All lines provided by Codecademy
+# All lines provided by Codecademy. Modified by John Renodin
 #Your code below:
+incoming_class =[
+  ["Kenny", "American", 9],
+  ["Tanya", "Ukrainian", 9],
+  ["Madison", "Indian", 7]
+]
+print(incoming_class)
