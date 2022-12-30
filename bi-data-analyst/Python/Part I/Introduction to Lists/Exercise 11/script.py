d1ca643e7709bd969fe8c0daa4a36f1b7df6c319
J0hnRjr <jlrenodin@liberty.edu>
2022-12-30 13:54:37 -0500

    Python/Lists/Exercise 11 step 2

--- bi-data-analyst/Python/Part I/Introduction to Lists/Exercise 11/script.py
+++ bi-data-analyst/Python/Part I/Introduction to Lists/Exercise 11/script.py
@@ -1,3 +1,4 @@
 # All lines provided by Codecademy. Modified by John Renodin
 heights = [["Jenny", 61], ["Alexus", 70], ["Sam", 67], ["Grace", 64]]
 heights.append(["Vik", 68])
+ages = [["Aaron", 15], ["Dhruti", 16]]
