e4291344b2a25b88068b7c6bb78be980a79fa657
J0hnRjr <jlrenodin@liberty.edu>
2022-12-30 15:13:28 -0500

    Python/Lists/Project 1 step 2

--- bi-data-analyst/Python/Part I/Introduction to Lists/Project 1/script.py
+++ bi-data-analyst/Python/Part I/Introduction to Lists/Project 1/script.py
@@ -1,4 +1,4 @@
-# All lines provided by Codecademy
+# All lines provided by Codecademy. Modified by John Renodin
 # Function to estimate insurance cost:
 def estimate_insurance_cost(name, age, sex, bmi, num_of_children, smoker):
   estimated_cost = 250*age - 128*sex + 370*bmi + 425*num_of_children + 24000*smoker - 12500
@@ -16,3 +16,4 @@ rohan_insurance_cost = estimate_insurance_cost(name =
 valentina_insurance_cost = estimate_insurance_cost(name = "Valentina", age = 53, sex = 0, bmi = 31.4, num_of_children = 0, smoker = 1)
 
 # Add your code here
+names = ["Maria", "Rohan", "Valentina"]
