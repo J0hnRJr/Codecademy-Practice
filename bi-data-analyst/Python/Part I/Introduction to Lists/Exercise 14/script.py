e2b80e297ab1a0cb39aaeb42b6402750cf248f4a
J0hnRjr <jlrenodin@liberty.edu>
2022-12-30 15:01:19 -0500

    Python/Lists/Exercise 14 step 4

--- bi-data-analyst/Python/Part I/Introduction to Lists/Exercise 14/script.py
+++ bi-data-analyst/Python/Part I/Introduction to Lists/Exercise 14/script.py
@@ -4,3 +4,10 @@ first_names = ['Ainsley', 'Ben', 'Chani', 'Depak']
 preferred_size = ["Small", "Large", "Medium"]
 preferred_size.append("Medium")
 print(preferred_size)
+customer_data = [
+  ['Ainsley', 'Small', True],
+  ['Ben', 'Large', False],
+  ['Chani', 'Medium', True],
+  ['Depak', 'Medium', False]
+]
+print(customer_data)
