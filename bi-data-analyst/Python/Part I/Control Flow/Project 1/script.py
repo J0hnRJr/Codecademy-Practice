51800829c5e121ee1f67de3b2c05ff561069c6e9
J0hnRjr <jlrenodin@liberty.edu>
2022-12-30 13:05:19 -0500

    Python/Control Flow/Project 1 step 7

--- bi-data-analyst/Python/Part I/Control Flow/Project 1/script.py
+++ bi-data-analyst/Python/Part I/Control Flow/Project 1/script.py
@@ -7,6 +7,16 @@ def analyze_smoker(smoker_status):
     print("Smoking is not an issue for you.")
 def analyze_bmi(bmi_value):
   print(bmi_value)
+
+def analyze_bmi(bmi_value):
+  if bmi_value > 30:
+    print("Your BMI is in the obese range. To lower your cost, you should significantly lower your BMI.")
+  elif bmi_value >= 25:
+    print("Your BMI is in the overweight range. To lower your cost, you should lower your BMI")
+  elif bmi_value >= 18.5:
+    print("Your BMI is in a healthy range")
+  else:
+    print("Your BMI is in the underweight range. Increasing your BMI will not help lower your cost, but it will help improve your health.")
 # Function to estimate insurance cost:
 def estimate_insurance_cost(name, age, sex, bmi, num_of_children, smoker):
   estimated_cost = 250*age - 128*sex + 370*bmi + 425*num_of_children + 24000*smoker - 12500
