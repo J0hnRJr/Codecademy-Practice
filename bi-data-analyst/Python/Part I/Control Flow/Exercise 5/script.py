a93625194934c70f9535b2bacc700482d662c9e3
J0hnRjr <jlrenodin@liberty.edu>
2022-12-30 11:36:13 -0500

    Python/Control Flow/Exercise 5 step 1

--- bi-data-analyst/Python/Part I/Control Flow/Exercise 5/script.py
+++ bi-data-analyst/Python/Part I/Control Flow/Exercise 5/script.py
@@ -1,11 +1,11 @@
-# All lines provided by Codecademy
+# All lines provided by Codecademy. Modified by John Renodin
 
 x = 20
 y = 20
 
 # Write the first if statement here:
-
-
+if x == y:
+  print("These numbers are the same")
 
 credits = 120
 
