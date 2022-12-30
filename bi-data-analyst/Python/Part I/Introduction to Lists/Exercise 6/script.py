65858dbedd192ffbef7d89ed2fc5d84eb15785a8
J0hnRjr <jlrenodin@liberty.edu>
2022-12-30 13:31:55 -0500

    Python/Lists/Exercise 6 step 3

--- bi-data-analyst/Python/Part I/Introduction to Lists/Exercise 6/script.py
+++ bi-data-analyst/Python/Part I/Introduction to Lists/Exercise 6/script.py
@@ -4,5 +4,4 @@ orders = ["daisy", "buttercup", "snapdragon", "gardenia", "lily"]
 # Create new orders here:
 new_orders= ["lilac", "iris"]
 orders_combined = orders + new_orders
-
-#broken_prices = [5, 3, 4, 5, 4] + 4
+broken_prices = [5, 3, 4, 5, 4] + [4]
