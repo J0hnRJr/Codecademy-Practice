7cc02e2302cb1ea7f83304f3733015da38ae00f3
J0hnRjr <jlrenodin@liberty.edu>
2022-12-30 12:33:49 -0500

    Python/Control Flow/Exercise 11 step 0

---
+++ bi-data-analyst/Python/Part I/Control Flow/Exercise 11/script.py
@@ -0,0 +1,11 @@
+# All lines provided by Codecademy
+print("I have information for the following planets:\n")
+
+print("   1. Venus   2. Mars    3. Jupiter")
+print("   4. Saturn  5. Uranus  6. Neptune\n")
+ 
+weight = 185
+planet = 3
+
+# Write an if statement below:
+
