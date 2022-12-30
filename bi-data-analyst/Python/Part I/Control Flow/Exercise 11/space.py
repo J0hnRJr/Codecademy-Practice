1f3af4172ee42a9e443931f90b18eb40d1d1c247
J0hnRjr <jlrenodin@liberty.edu>
2022-12-30 12:43:55 -0500

    Python/Control Flow/Exercise 11 step 1 rename

---
+++ bi-data-analyst/Python/Part I/Control Flow/Exercise 11/space.py
@@ -0,0 +1,22 @@
+# All lines provided by Codecademy
+print("I have information for the following planets:\n")
+
+print("   1. Venus   2. Mars    3. Jupiter")
+print("   4. Saturn  5. Uranus  6. Neptune\n")
+ 
+weight = 185
+planet = 3
+
+# Write an if statement below:
+if planet == 1:
+  print("On Venus you weigh " + str(0.91 * weight))
+elif planet == 2:
+  print("Mars you weigh " + str(0.38 * weight))
+elif planet == 3:
+  print("Jupiter you weigh " + str(2.34 * weight))
+elif planet == 4:
+  print("Saturn you weigh " + str(1.06 * weight))
+elif planet == 5:
+  print("Uranus you weigh " + str(0.92 * weight))
+elif planet == 6:
+  print("Neptune you weigh " + str(1.19 * weight))
