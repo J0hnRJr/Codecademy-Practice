06ebe180da8a159dcaa34f6614ea663495ce72df
J0hnRjr <jlrenodin@liberty.edu>
2022-12-30 11:32:08 -0500

    Python/Control Flow/Exercise 4 step 1

--- bi-data-analyst/Python/Part I/Control Flow/Exercise 4/script.py
+++ bi-data-analyst/Python/Part I/Control Flow/Exercise 4/script.py
@@ -1,6 +1,6 @@
 # All lines provided by Codecademy
 # Enter a user name here, make sure to make it a string
-user_name = 
+user_name = "John"
 
 if user_name = "Dave":
   print("Get off my computer Dave!")
