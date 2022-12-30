689bc4228c0791894a13aba4b7cce12258fcfcd9
J0hnRjr <jlrenodin@liberty.edu>
2022-12-30 11:57:49 -0500

    Python/Control Flow/Exercise 8 step 2

--- bi-data-analyst/Python/Part I/Control Flow/Exercise 8/script.py
+++ bi-data-analyst/Python/Part I/Control Flow/Exercise 8/script.py
@@ -6,3 +6,11 @@ statement_two = not (8 * 2) != 20 - 4
 credits = 120
 gpa = 1.8
 
+if not(credits >= 120):
+  print("You do not have enough credits to graduate.")  
+
+if not(gpa >= 2.0):
+  print("Your GPA is not high enough to graduate.")
+
+if not(credits >= 120) and not(gpa >= 2.0):
+  print("You do not meet either requirement to graduate!")
