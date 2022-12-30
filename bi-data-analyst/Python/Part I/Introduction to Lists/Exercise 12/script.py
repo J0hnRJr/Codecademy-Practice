973b29637e12b80ded83c18b6d3070b32477cb4f
J0hnRjr <jlrenodin@liberty.edu>
2022-12-30 13:57:13 -0500

    Python/Lists/Exercise 12 step 1

--- bi-data-analyst/Python/Part I/Introduction to Lists/Exercise 12/script.py
+++ bi-data-analyst/Python/Part I/Introduction to Lists/Exercise 12/script.py
@@ -1,2 +1,9 @@
-# All lines provided by Codecademy
+# All lines provided by Codecademy. Modified by John Renodin.
 #Your code below:
+class_name_test = [
+  ["Jenny", 90],
+  ["Alexus",	85.5],
+  ["Sam",	83],
+  ["Ellie", 101.5]
+]
+print(class_name_test)
