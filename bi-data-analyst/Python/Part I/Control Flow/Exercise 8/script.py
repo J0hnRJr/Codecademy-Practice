a3fe9e285a0ecf65c8efc381b8d04be049932a21
J0hnRjr <jlrenodin@liberty.edu>
2022-12-30 11:55:37 -0500

    Python/Control Flow/Exercise 8 step 1

--- bi-data-analyst/Python/Part I/Control Flow/Exercise 8/script.py
+++ bi-data-analyst/Python/Part I/Control Flow/Exercise 8/script.py
@@ -1,7 +1,7 @@
-# All lines provided by Codecademy
-statement_one = 
+# All lines provided by Codecademy. Modified by John Renodin
+statement_one = not (4 + 5 <= 9)
 
-statement_two = 
+statement_two = not (8 * 2) != 20 - 4
 
 credits = 120
 gpa = 1.8
