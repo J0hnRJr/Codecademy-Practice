196ef1a8ffce9b74f362db9e1b7ba27ca7899016
J0hnRjr <jlrenodin@liberty.edu>
2022-12-30 13:45:47 -0500

    Python/Lists/Exercise 10 step 2

--- bi-data-analyst/Python/Part I/Introduction to Lists/Exercise 10/script.py
+++ bi-data-analyst/Python/Part I/Introduction to Lists/Exercise 10/script.py
@@ -1,4 +1,5 @@
 # All lines provided by Codecademy. Modified by John Renodin
 # Your code below: 
 order_list = ["Celery", "Orange Juice", "Orange", "Flatbread"]
+order_list.remove("Flatbread")
 print(order_list)
