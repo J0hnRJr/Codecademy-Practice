47b58a28a6e3d791687cda3877cb7fd1e4b6fe2c
J0hnRjr <jlrenodin@liberty.edu>
2022-12-30 12:58:37 -0500

    Python/Control Flow/Project 1 step 3

--- bi-data-analyst/Python/Part I/Control Flow/Project 1/script.py
+++ bi-data-analyst/Python/Part I/Control Flow/Project 1/script.py
@@ -1,6 +1,7 @@
-# All lines provided by Codecademy
+# All lines provided by Codecademy. Modified by John Renodin
 # Add your code here
- 
+def analyze_smoker(smoker_status):
+    print(smoker_status)
 # Function to estimate insurance cost:
 def estimate_insurance_cost(name, age, sex, bmi, num_of_children, smoker):
   estimated_cost = 250*age - 128*sex + 370*bmi + 425*num_of_children + 24000*smoker - 12500
