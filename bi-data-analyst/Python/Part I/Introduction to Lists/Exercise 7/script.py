cf7dcf65191385be7a5eb7eb11a36c39b7335525
J0hnRjr <jlrenodin@liberty.edu>
2022-12-30 13:36:26 -0500

    Python/Lists/Exercise 7 step 3

--- bi-data-analyst/Python/Part I/Introduction to Lists/Exercise 7/script.py
+++ bi-data-analyst/Python/Part I/Introduction to Lists/Exercise 7/script.py
@@ -1,4 +1,4 @@
 # All lines provided by Codecademy. Modified by John Renodin
 employees = ["Michael", "Dwight", "Jim", "Pam", "Ryan", "Andy", "Robert"]
 employee_four = employees[3]
-print(employees[8])
+print(employees[5])
