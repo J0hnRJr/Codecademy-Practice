3103d4e25a90320cd579df5e6f3db38151b67c55
J0hnRjr <jlrenodin@liberty.edu>
2022-12-30 13:10:41 -0500

    update for python lists section

--- writeFiles.py
+++ writeFiles.py
@@ -1,9 +1,9 @@
 # A simple tool to create files written by John Renodin
 import os
-parentDir = 'c:/dev/Codecademy-Practice/bi-data-analyst/Python/Part I/Control Flow'
+parentDir = 'c:/dev/Codecademy-Practice/bi-data-analyst/Python/Part I/Introduction to Lists'
 exerciseNum = 1
-for num in range(exerciseNum, 12):
+for num in range(exerciseNum, 15):
     os.mkdir(parentDir + '/Exercise ' + str(num))
     f = open(parentDir + '/Exercise ' + str(num) + '/' + "script.py", "w")
-    f.write('# All lines written by John Renodin\n')
+    f.write('# All lines provided by Codecademy\n')
     f.close()
