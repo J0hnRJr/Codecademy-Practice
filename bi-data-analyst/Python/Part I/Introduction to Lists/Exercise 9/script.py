83d1a04eb6dd27fee6bf7ca9d9fcdb690eb15af6
J0hnRjr <jlrenodin@liberty.edu>
2022-12-30 13:42:23 -0500

    Python/Lists/Exercise 9 step 2

--- bi-data-analyst/Python/Part I/Introduction to Lists/Exercise 9/script.py
+++ bi-data-analyst/Python/Part I/Introduction to Lists/Exercise 9/script.py
@@ -1,3 +1,5 @@
 # All lines provided by Codecademy. Modified by John Renodin
 # Your code below:
 garden_waitlist = ["Jiho", "Adam", "Sonny", "Alisha"]
+garden_waitlist[1] = "Calla"
+print(garden_waitlist)
