d85127cc04ef6cdd6369a58883ac362161610f07
J0hnRjr <jlrenodin@liberty.edu>
2022-12-30 11:52:43 -0500

    Python/Control Flow/Exercise 7 step 1

--- bi-data-analyst/Python/Part I/Control Flow/Exercise 7/script.py
+++ bi-data-analyst/Python/Part I/Control Flow/Exercise 7/script.py
@@ -1,7 +1,7 @@
 # All lines provided by Codecademys
-statement_one = 
+statement_one = (2 - 1 > 3) or (-5 * 2 == -10)
 
-statement_two = 
+statement_two = (2 - 1 > 3) or (-5 * 2 == -10)
 
 credits = 118
 gpa = 2.0
