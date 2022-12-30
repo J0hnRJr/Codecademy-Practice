0c7429d360c211d5abfe925749bfe1a4907a8bd9
J0hnRjr <jlrenodin@liberty.edu>
2022-12-30 12:45:47 -0500

    Python/Control Flow/Project 1 step 1

---
+++ bi-data-analyst/Python/Part I/Control Flow/Project 1/script.py
@@ -0,0 +1,11 @@
+# All lines provided by Codecademy
+# Add your code here
+ 
+# Function to estimate insurance cost:
+def estimate_insurance_cost(name, age, sex, bmi, num_of_children, smoker):
+  estimated_cost = 250*age - 128*sex + 370*bmi + 425*num_of_children + 24000*smoker - 12500
+  print(name + "'s Estimated Insurance Cost: " + str(estimated_cost) + " dollars.")
+  return estimated_cost
+ 
+# Estimate Keanu's insurance cost
+keanu_insurance_cost = estimate_insurance_cost(name = 'Keanu', age = 29, sex = 1, bmi = 26.2, num_of_children = 3, smoker = 1)
