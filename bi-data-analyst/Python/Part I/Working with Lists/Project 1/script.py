13bc774e9818a9655f06834f6a470f55100a642f
J0hnRjr <jlrenodin@liberty.edu>
2023-01-04 10:28:52 -0500

    Python/Working with Lists/Project 1 step 0

---
+++ bi-data-analyst/Python/Part I/Working with Lists/Project 1/script.py
@@ -0,0 +1,5 @@
+# All lines provided by Codecademy
+names = ["Mohamed", "Sara", "Xia", "Paul", "Valentina", "Jide", "Aaron", "Emily", "Nikita", "Paul"]
+insurance_costs = [13262.0, 4816.0, 6839.0, 5054.0, 14724.0, 5360.0, 7640.0, 6072.0, 2750.0, 12064.0]
+
+# Add your code here
