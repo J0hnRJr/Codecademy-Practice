9e2c2d82f175fecaf500e9aa72885370d21e8c19
J0hnRjr <jlrenodin@liberty.edu>
2023-01-04 10:11:46 -0500

    Python/Working with Lists/Exercise 11 step 0

---
+++ bi-data-analyst/Python/Part I/Working with Lists/Exercise 11/script.py
@@ -0,0 +1,2 @@
+# All lines provided by Codecademy
+inventory = ["twin bed", "twin bed", "headboard", "queen bed", "king bed", "dresser", "dresser", "table", "table", "nightstand", "nightstand", "king bed", "king bed", "twin bed", "twin bed", "sheets", "sheets", "pillow", "pillow"]
