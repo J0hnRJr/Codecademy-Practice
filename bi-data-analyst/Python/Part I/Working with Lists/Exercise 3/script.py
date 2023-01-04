3dd6fde251f77036a228642da7eb23ceb2d1f725
J0hnRjr <jlrenodin@liberty.edu>
2023-01-04 09:38:33 -0500

    Python/Working with Lists/Exercise 3 step 1

--- bi-data-analyst/Python/Part I/Working with Lists/Exercise 3/script.py
+++ bi-data-analyst/Python/Part I/Working with Lists/Exercise 3/script.py
@@ -1,5 +1,5 @@
-# All lines provided by Codecademy
+# All lines provided by Codecademy. Modified by John Renodin
 # Your code below: 
 
-number_list = range(3)
+number_list = range(9)
 print(list(number_list))
