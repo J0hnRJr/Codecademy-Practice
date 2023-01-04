72085c1046169b38b79de4aaf383686e17983fc6
J0hnRjr <jlrenodin@liberty.edu>
2023-01-04 10:16:55 -0500

    Python/Working with Lists/Exercise 11 step 3

--- bi-data-analyst/Python/Part I/Working with Lists/Exercise 11/script.py
+++ bi-data-analyst/Python/Part I/Working with Lists/Exercise 11/script.py
@@ -2,3 +2,4 @@
 inventory = ["twin bed", "twin bed", "headboard", "queen bed", "king bed", "dresser", "dresser", "table", "table", "nightstand", "nightstand", "king bed", "king bed", "twin bed", "twin bed", "sheets", "sheets", "pillow", "pillow"]
 inventory_len = len(inventory)
 first = inventory[0]
+last = inventory[-1]
