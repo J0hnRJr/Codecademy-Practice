e23c232ce3f5cdd30e83416b40ab76ccfe779163
J0hnRjr <jlrenodin@liberty.edu>
2023-01-04 11:20:51 -0500

    Python/Loops/Exercise 6 step 0

---
+++ bi-data-analyst/Python/Part I/Loops/Exercise 6/script.py
@@ -0,0 +1,7 @@
+# All lines provided by Codecademy
+students_period_A = ["Alex", "Briana", "Cheri", "Daniele"]
+students_period_B = ["Dora", "Minerva", "Alexa", "Obie"]
+
+for student in students_period_A:
+  #students_period_A.append(student)
+  print(student)
