f5fcbcc306fa7878fcd4e9257dc40ff888a9eb19
J0hnRjr <jlrenodin@liberty.edu>
2023-01-04 11:17:16 -0500

    Python/Loops/Exercise 5 step 1

--- bi-data-analyst/Python/Part I/Loops/Exercise 5/script.py
+++ bi-data-analyst/Python/Part I/Loops/Exercise 5/script.py
@@ -1,4 +1,5 @@
-# All lines provided by Codecademy
+# All lines provided by Codecademy. Modified by John Renodin.
 python_topics = ["variables", "control flow", "loops", "modules", "classes"]
 
 #Your code below: 
+length = len(python_topics)
