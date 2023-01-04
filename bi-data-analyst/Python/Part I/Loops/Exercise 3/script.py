fafcc0f9b114fa56c987a2a39e3bc74a2345a686
J0hnRjr <jlrenodin@liberty.edu>
2023-01-04 11:08:49 -0500

    Python/Loops/Exercise 3 step 1

--- bi-data-analyst/Python/Part I/Loops/Exercise 3/script.py
+++ bi-data-analyst/Python/Part I/Loops/Exercise 3/script.py
@@ -1,2 +1,4 @@
-# All lines provided by Codecademy
+# All lines provided by Codecademy. Modified by John Renodin.
 promise = "I will finish the python loops module!"
+for count in range(5):
+  print(promise)
