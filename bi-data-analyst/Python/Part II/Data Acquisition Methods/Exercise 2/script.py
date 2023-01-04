b466eccfa420678b9b1e4006790efabd65317e03
J0hnRjr <jlrenodin@liberty.edu>
2023-01-04 14:35:47 -0500

    Python/Data Acquisition Methods/Exercise 2 step 1

--- bi-data-analyst/Python/Part II/Data Acquisition Methods/Exercise 2/script.py
+++ bi-data-analyst/Python/Part II/Data Acquisition Methods/Exercise 2/script.py
@@ -1,5 +1,5 @@
 # All lines provided by Codecademy
 import requests
-
+import csv
 
 r = requests.get('https://api.census.gov/data/2020/acs/acs5?get=NAME,B08303_001E,B08303_013E&for=county:*&in=state:36')
