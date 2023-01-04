80680a33d5e4d027e9e5f65a25224ee98059cbaf
J0hnRjr <jlrenodin@liberty.edu>
2023-01-04 14:32:15 -0500

    Python/Data Acquisition Methods/Exercise 1 step 4

--- bi-data-analyst/Python/Part II/Data Acquisition Methods/Exercise 1/script.py
+++ bi-data-analyst/Python/Part II/Data Acquisition Methods/Exercise 1/script.py
@@ -3,4 +3,6 @@ import requests
 
 r = requests.get('https://api.census.gov/data/2020/acs/acs5?get=NAME,B08303_001E,B08303_013E&for=county:*&in=state:36')
 r_text = r.text
+r_json = r.json()
 print(r_text)
+print(r_json)
