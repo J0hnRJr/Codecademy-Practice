216d0d3e0704f9b3942e5728c5afd45cc492a219
J0hnRjr <jlrenodin@liberty.edu>
2023-01-04 09:48:06 -0500

    Python/Working with Lists/Exercise 5 step 5

--- bi-data-analyst/Python/Part I/Working with Lists/Exercise 5/script.py
+++ bi-data-analyst/Python/Part I/Working with Lists/Exercise 5/script.py
@@ -1,7 +1,7 @@
 # All lines provided by Codecademy. Modified by John Renodin.
 long_list = [1, 5, 6, 7, -23, 69.5, True, "very", "long", "list", "that", "keeps", "going.", "Let's", "practice", "getting", "the", "length"]
 
-big_range = range(2, 3000, 10)
+big_range = range(2, 3000, 100)
 
 # Your code below: 
 long_list_len = len(long_list)
