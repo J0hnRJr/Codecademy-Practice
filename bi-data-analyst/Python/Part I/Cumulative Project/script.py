a2d9e4eaadbb77405326ad643a8d75f62aefb72b
J0hnRjr <jlrenodin@liberty.edu>
2023-01-04 14:14:41 -0500

    Python/Cumulative Project step 4

--- bi-data-analyst/Python/Part I/Cumulative Project/script.py
+++ bi-data-analyst/Python/Part I/Cumulative Project/script.py
@@ -3,3 +3,7 @@ paintings = ['The Two Fridas', 'My Dress Hangs Here', 'Tree of Hope', 'Self Port
 dates = [1939, 1933, 1946, 1940]
 paintings = list(zip(paintings, dates))
 print(paintings)
+paintings.append(['The Broken Column', 1944])
+paintings.append(['The Wounded Deer', 1946])
+paintings.append(['Me and My Doll', 1937])
+print(paintings)
