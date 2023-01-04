df6ffa7d2aeed2c4af25339605adc166ab49413b
J0hnRjr <jlrenodin@liberty.edu>
2023-01-04 09:49:52 -0500

    Python/Working with Lists/Exercise 6 step 2

--- bi-data-analyst/Python/Part I/Working with Lists/Exercise 6/script.py
+++ bi-data-analyst/Python/Part I/Working with Lists/Exercise 6/script.py
@@ -1,7 +1,7 @@
 # All lines provided by Codecademy. Modified by John Renodin.
 suitcase = ["shirt", "shirt", "pants", "pants", "pajamas", "books"]
 
-beginning = suitcase[0:4]
+beginning = suitcase[0:2]
 
 # Your code below: 
 print(beginning)
