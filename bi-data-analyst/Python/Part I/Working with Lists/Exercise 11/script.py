af0fc2be5f60f2222b8e1c28d2a4ca07d4fa0f38
J0hnRjr <jlrenodin@liberty.edu>
2023-01-04 10:16:30 -0500

    Python/Working with Lists/Exercise 11 step 1

--- bi-data-analyst/Python/Part I/Working with Lists/Exercise 11/script.py
+++ bi-data-analyst/Python/Part I/Working with Lists/Exercise 11/script.py
@@ -1,2 +1,3 @@
-# All lines provided by Codecademy
+# All lines provided by Codecademy. Modified by John Renodin
 inventory = ["twin bed", "twin bed", "headboard", "queen bed", "king bed", "dresser", "dresser", "table", "table", "nightstand", "nightstand", "king bed", "king bed", "twin bed", "twin bed", "sheets", "sheets", "pillow", "pillow"]
+inventory_len = len(inventory)
