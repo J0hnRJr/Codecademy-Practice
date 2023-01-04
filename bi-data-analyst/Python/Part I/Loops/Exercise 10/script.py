a50081910af19143b3fd042c66e9cc76820ccaef
J0hnRjr <jlrenodin@liberty.edu>
2023-01-04 11:32:53 -0500

    Python/Loops/Exercise 10 step 1

--- bi-data-analyst/Python/Part I/Loops/Exercise 10/script.py
+++ bi-data-analyst/Python/Part I/Loops/Exercise 10/script.py
@@ -1,2 +1,3 @@
-# All lines provided by Codecademy
+# All lines provided by Codecademy. Modified by John Renodin
 grades = [90, 88, 62, 76, 74, 89, 48, 57]
+scaled_grades = [grade + 10 for grade in grades]
