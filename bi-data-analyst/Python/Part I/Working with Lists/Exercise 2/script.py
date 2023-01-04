adfae2ab67ff997a7ae8497415b5ca2782bedfb8
J0hnRjr <jlrenodin@liberty.edu>
2023-01-04 09:35:12 -0500

    Python/Working with Lists/Exercise 2 step 2

--- bi-data-analyst/Python/Part I/Working with Lists/Exercise 2/script.py
+++ bi-data-analyst/Python/Part I/Working with Lists/Exercise 2/script.py
@@ -1,8 +1,7 @@
-# All lines provided by Codecademy
+# All lines provided by Codecademy. Modified by John Renodin
 data_science_topics = ["Machine Learning", "SQL", "Pandas", "Algorithms", "Statistics", "Python 3"]
 print(data_science_topics)
 
 # Your code below: 
-
-
-
+data_science_topics.pop()
+print(data_science_topics)
