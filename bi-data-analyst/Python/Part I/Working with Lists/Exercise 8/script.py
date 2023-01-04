774448c6f46a8fe032b6a97e78a6bf99ae4fae31
J0hnRjr <jlrenodin@liberty.edu>
2023-01-04 09:57:43 -0500

    Python/Working with Lists/Exercise 8 add modified statement

--- bi-data-analyst/Python/Part I/Working with Lists/Exercise 8/script.py
+++ bi-data-analyst/Python/Part I/Working with Lists/Exercise 8/script.py
@@ -1,4 +1,4 @@
-# All lines provided by Codecademy
+# All lines provided by Codecademy. Modified by John Renodin.
 votes = ["Jake", "Jake", "Laurie", "Laurie", "Laurie", "Jake", "Jake", "Jake", "Laurie", "Cassie", "Cassie", "Jake", "Jake", "Cassie", "Laurie", "Cassie", "Jake", "Jake", "Cassie", "Laurie"]
 
 # Your code below: 
