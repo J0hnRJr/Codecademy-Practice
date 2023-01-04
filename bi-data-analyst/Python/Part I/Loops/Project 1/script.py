5d39b15f053a3180146cdc2ed522035d06ed00a2
J0hnRjr <jlrenodin@liberty.edu>
2023-01-04 13:50:33 -0500

    Python/Loops/Project 1 step 1

--- bi-data-analyst/Python/Part I/Loops/Project 1/script.py
+++ bi-data-analyst/Python/Part I/Loops/Project 1/script.py
@@ -4,3 +4,4 @@ estimated_insurance_costs = [1000.0, 2000.0, 3000.0, 4000.0, 5000.0, 6000.0, 700
 actual_insurance_costs = [1100.0, 2200.0, 3300.0, 4400.0, 5500.0, 6600.0, 7700.0]
 
 # Add your code here
+total_cost = 0
