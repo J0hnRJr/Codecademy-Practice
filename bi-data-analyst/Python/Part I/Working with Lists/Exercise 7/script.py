56def797b5289817b63e03b1667df9e91b0a47c8
J0hnRjr <jlrenodin@liberty.edu>
2023-01-04 09:53:58 -0500

    Python/Working with Lists/Exercise 7 step 1

--- bi-data-analyst/Python/Part I/Working with Lists/Exercise 7/script.py
+++ bi-data-analyst/Python/Part I/Working with Lists/Exercise 7/script.py
@@ -1,4 +1,6 @@
-# All lines provided by Codecademy
+# All lines provided by Codecademy. Modified by John Renodin
 suitcase = ["shirt", "shirt", "pants", "pants", "pajamas", "books"]
 
 # Your code below: 
+last_two_elements = suitcase[-2:]
+print(last_two_elements)
