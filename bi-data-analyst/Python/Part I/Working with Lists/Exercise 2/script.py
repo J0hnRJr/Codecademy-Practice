1647e0b05482f6f27bbb9bacffb416d09500b920
J0hnRjr <jlrenodin@liberty.edu>
2023-01-04 09:33:59 -0500

    Python/Working with Lists/Exercise 2 step 1

---
+++ bi-data-analyst/Python/Part I/Working with Lists/Exercise 2/script.py
@@ -0,0 +1,8 @@
+# All lines provided by Codecademy
+data_science_topics = ["Machine Learning", "SQL", "Pandas", "Algorithms", "Statistics", "Python 3"]
+print(data_science_topics)
+
+# Your code below: 
+
+
+
