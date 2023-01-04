ed545d8fa995f525c6e1331911cdcae639aa81df
J0hnRjr <jlrenodin@liberty.edu>
2023-01-04 10:30:13 -0500

    Python/Working with Lists/Project 1 step 1

--- bi-data-analyst/Python/Part I/Working with Lists/Project 1/script.py
+++ bi-data-analyst/Python/Part I/Working with Lists/Project 1/script.py
@@ -1,5 +1,7 @@
-# All lines provided by Codecademy
+# All lines provided by Codecademy. Modified by John Renodin.
 names = ["Mohamed", "Sara", "Xia", "Paul", "Valentina", "Jide", "Aaron", "Emily", "Nikita", "Paul"]
 insurance_costs = [13262.0, 4816.0, 6839.0, 5054.0, 14724.0, 5360.0, 7640.0, 6072.0, 2750.0, 12064.0]
 
 # Add your code here
+names.append("Priscilla")
+insurance_costs.append(8320.0)
