fbb662146348b0039bf3e0927af4a25233766275
J0hnRjr <jlrenodin@liberty.edu>
2023-01-04 10:00:47 -0500

    Python/Working with Lists/Exercise 9 step 3

--- bi-data-analyst/Python/Part I/Working with Lists/Exercise 9/script.py
+++ bi-data-analyst/Python/Part I/Working with Lists/Exercise 9/script.py
@@ -5,7 +5,7 @@ addresses = ["221 B Baker St.", "42 Wallaby Way", "12 Grimmauld Place", "742 Eve
 
 # Checkpoint 3
 names = ["Ron", "Hermione", "Harry", "Albus", "Sirius"]
-#sort(names)
+names.sort()
 
 
 # Checkpoint 4 & 5
