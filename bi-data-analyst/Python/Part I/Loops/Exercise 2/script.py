9c2cb7ec4b24e4d86cdea2fba3c3f66fea1e8f43
J0hnRjr <jlrenodin@liberty.edu>
2023-01-04 11:04:54 -0500

    Python/Loops/Exercise 2 step 1

---
+++ bi-data-analyst/Python/Part I/Loops/Exercise 2/script.py
@@ -0,0 +1,7 @@
+# All lines provided by Codecademy
+board_games = ["Settlers of Catan", "Carcassone", "Power Grid", "Agricola", "Scrabble"]
+
+sport_games = ["football", "hockey", "baseball", "cricket"]
+
+for game in board_games:
+print(game)
