d4888c2c1e479fbc3f94eb741db5996ed6a88ea2
J0hnRjr <jlrenodin@liberty.edu>
2023-01-04 11:50:35 -0500

    Python/Loops/Project 1 step 0

---
+++ bi-data-analyst/Python/Part I/Loops/Project 1/script.py
@@ -0,0 +1,5 @@
+names = ["Judith", "Abel", "Tyson", "Martha", "Beverley", "David", "Anabel"]
+estimated_insurance_costs = [1000.0, 2000.0, 3000.0, 4000.0, 5000.0, 6000.0, 7000.0]
+actual_insurance_costs = [1100.0, 2200.0, 3300.0, 4400.0, 5500.0, 6600.0, 7700.0]
+
+# Add your code here
