c702dd8bd92187e19ee2911955ad1231d8275660
J0hnRjr <jlrenodin@liberty.edu>
2023-01-04 09:56:30 -0500

    Python/Working with Lists/Exercise 8 step 1

--- bi-data-analyst/Python/Part I/Working with Lists/Exercise 8/script.py
+++ bi-data-analyst/Python/Part I/Working with Lists/Exercise 8/script.py
@@ -2,3 +2,4 @@
 votes = ["Jake", "Jake", "Laurie", "Laurie", "Laurie", "Jake", "Jake", "Jake", "Laurie", "Cassie", "Cassie", "Jake", "Jake", "Cassie", "Laurie", "Cassie", "Jake", "Jake", "Cassie", "Laurie"]
 
 # Your code below: 
+jake_votes = votes.count("Jake")
