20989e460f77fc09eb1633addc6aea2377deed1b
J0hnRjr <jlrenodin@liberty.edu>
2023-01-04 11:22:41 -0500

    Python/Loops/Exercise 7 step 1

--- bi-data-analyst/Python/Part I/Loops/Exercise 7/script.py
+++ bi-data-analyst/Python/Part I/Loops/Exercise 7/script.py
@@ -1,4 +1,6 @@
-# All lines provided by Codecademy
+# All lines provided by Codecademy. Modified by John Renodin.
 dog_breeds_available_for_adoption = ["french_bulldog", "dalmatian", "shihtzu", "poodle", "collie"]
 dog_breed_I_want = "dalmatian"
 
+for dog_breed in dog_breeds_available_for_adoption:
+  print(dog_breed)
