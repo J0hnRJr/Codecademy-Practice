4ce60db53c419fc1906a9fbc363e632648053fbb
J0hnRjr <jlrenodin@liberty.edu>
2023-01-04 11:11:09 -0500

    Python/Loops/Exercise 4 step 1

---
+++ bi-data-analyst/Python/Part I/Loops/Exercise 4/script.py
@@ -0,0 +1,16 @@
+# All lines provided by Codecademy. Modified by John Renodin
+# # While Loop Walkthrough
+# count = 0
+# print("Starting While Loop")
+# while count <= 3:
+#   # Loop Body
+#   # Print if the condition is still true
+#   print("Loop Iteration - count <= 3 is still true")
+#   # Print the current value of count 
+#   print("Count is currently " + str(count))
+#   # Increment count
+#   count += 1
+#   print(" ----- ")
+# print("While Loop ended")
+
+# Your code below: 
