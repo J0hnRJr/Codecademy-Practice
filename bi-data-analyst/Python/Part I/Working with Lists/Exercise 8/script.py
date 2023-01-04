603b7f258097d94f78da2be43e7963bd252d4690
J0hnRjr <jlrenodin@liberty.edu>
2023-01-04 09:55:51 -0500

    Python/Working with Lists/Exercise 8 step 0

---
+++ bi-data-analyst/Python/Part I/Working with Lists/Exercise 8/script.py
@@ -0,0 +1,4 @@
+# All lines provided by Codecademy
+votes = ["Jake", "Jake", "Laurie", "Laurie", "Laurie", "Jake", "Jake", "Jake", "Laurie", "Cassie", "Cassie", "Jake", "Jake", "Cassie", "Laurie", "Cassie", "Jake", "Jake", "Cassie", "Laurie"]
+
+# Your code below: 
