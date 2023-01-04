5a7d47373dd4273b4d660d3ab199a99519540e23
J0hnRjr <jlrenodin@liberty.edu>
2023-01-04 11:01:27 -0500

    Python/Loops/Exercise 1 step 1

--- bi-data-analyst/Python/Part I/Loops/Exercise 1/script.py
+++ bi-data-analyst/Python/Part I/Loops/Exercise 1/script.py
@@ -1,2 +1,12 @@
 # All lines provided by Codecademy
 # Write 10 print() statements below! 
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
