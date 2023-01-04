60f89fc24f3ab9fe36d040e0f220f54c8aa94a62
J0hnRjr <jlrenodin@liberty.edu>
2023-01-04 09:44:29 -0500

    Python/Working with Lists/Exercise 5 step 0

---
+++ bi-data-analyst/Python/Part I/Working with Lists/Exercise 5/script.py
@@ -0,0 +1,6 @@
+# All lines provided by Codecademy
+long_list = [1, 5, 6, 7, -23, 69.5, True, "very", "long", "list", "that", "keeps", "going.", "Let's", "practice", "getting", "the", "length"]
+
+big_range = range(2, 3000, 10)
+
+# Your code below: 
