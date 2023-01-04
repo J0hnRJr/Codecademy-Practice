024930faaa7c6d7ebe5a0818877488cb09196178
J0hnRjr <jlrenodin@liberty.edu>
2023-01-04 14:13:19 -0500

    Python/Cumulative Project step 3

--- bi-data-analyst/Python/Part I/Cumulative Project/script.py
+++ bi-data-analyst/Python/Part I/Cumulative Project/script.py
@@ -1,3 +1,5 @@
 # All lines written by John Renodin
 paintings = ['The Two Fridas', 'My Dress Hangs Here', 'Tree of Hope', 'Self Portrait With Monkeys']
 dates = [1939, 1933, 1946, 1940]
+paintings = list(zip(paintings, dates))
+print(paintings)
