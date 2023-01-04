7e9f19857a4f1bccb360e25b7e0c2f978859a570
J0hnRjr <jlrenodin@liberty.edu>
2023-01-04 11:36:45 -0500

    Python/Loops/Exercise 11 step 2

--- bi-data-analyst/Python/Part I/Loops/Exercise 11/script.py
+++ bi-data-analyst/Python/Part I/Loops/Exercise 11/script.py
@@ -1,3 +1,4 @@
 # All lines provided by Codecademy. Modified by John Renodin
 heights = [161, 164, 156, 144, 158, 170, 163, 163, 157]
 can_ride_coaster = [height for height in heights if height > 161]
+print(can_ride_coaster)
